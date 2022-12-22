a29de0c604a85867ca333082695a33e58a227aef
Sven <svenrisse0@gmail.com>
2022-12-22 10:30:40 +0100

    feat<trpc>: add newNote procedure

---
+++ src/server/trpc/router/mynotes.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { router, publicProcedure } from "../trpc";
+
+export const notesRouter = router({
+  //create a note
+  newNote: publicProcedure
+    .input(
+      z.object({
+        title: z
+          .string()
+          .min(5, { message: "Must be longer than 5 characters" })
+          .max(200, { message: "Must be shorter than 200 characters" })
+          .trim(),
+        description: z
+          .string()
+          .min(5, { message: "Must be longer than 5 characters" })
+          .max(200, { message: "Must be shorter than 200 characters" })
+          .trim(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        return await ctx.prisma.notes.create({
+          data: {
+            title: input.title,
+            description: input.description,
+          },
+        });
+      } catch (error) {
+        console.log(`Note cannot be created ${error}`);
+      }
+    }),
+});
