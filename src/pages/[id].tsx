e677d93cd5ee93a43e3c992cf4b766dcb5998ffa
Sven <svenrisse0@gmail.com>
2022-12-22 14:09:29 +0100

    style: add styling for note details

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -13,7 +13,7 @@ export default function NotesDetail() {
 
   return (
     <>
-      <main className="mx-auto flex min-h-screen flex-col py-10 sm:container">
+      <main className="mx-auto flex min-h-screen flex-col justify-center py-10 md:container">
         <Link
           className="indigo-700 inline-block py-4 text-base font-semibold leading-7 text-green-700"
           href="/"
@@ -23,9 +23,11 @@ export default function NotesDetail() {
         <h1 className="mb-8 text-3xl font-bold tracking-tight sm:text-left sm:text-3xl">
           Note details
         </h1>
-        <div className="mb-5">{messageDetail?.id}</div>
-        <h6 className="mb-5">{messageDetail?.title}</h6>
-        <p className="mb-5">{messageDetail?.description}</p>
+        <div className="flex flex-col rounded-lg bg-stone-100 p-5 text-xl">
+          <div className="mb-5 text-sm">Note ID: {messageDetail?.id}</div>
+          <h6 className="mb-5">Title: {messageDetail?.title}</h6>
+          <p className="mb-5">Description: {messageDetail?.description}</p>
+        </div>
       </main>
     </>
   );
